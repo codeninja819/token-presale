e6feb07a987a17c936299a11b3102790f6d1a429
codeninja819 <jiromatsumoto98@gmail.com>
2023-09-08 12:50:51 -0400

    update

--- front-end/src/components/contracts.ts
+++ front-end/src/components/contracts.ts
@@ -1,6 +1,6 @@
 import { erc20ABI } from 'wagmi'
 import { type Abi, type Address } from "viem";
-import PRESALE_ABI from "../abis/Presale.abi.json";
+import PRESALE_ABI from "../abis/presale.abi.json";
 
 export const TOKEN_CA = "0xB2eadDC5A2EeBBb71e89B70d97ce4f441a4DEf12";
 export const PRESALE_CA = "0x9bc7BCb71a11BaC10667b0c8Cc26803605606bFE";
