0df963432c15504007aefc200d27a136d77b3e37
codeninja819 <jiromatsumoto98@gmail.com>
2023-09-09 17:06:14 -0400

    update contarct

--- front-end/src/components/contracts.ts
+++ front-end/src/components/contracts.ts
@@ -3,7 +3,7 @@ import { type Abi, type Address } from "viem";
 import PRESALE_ABI from "../abis/presale.abi.json";
 
 export const TOKEN_CA = "0xb55bD1A63558E2384C2231eE8C2B8B036FF62727";
-export const PRESALE_CA = "0x3a1d4bD1289e79A2Eba64A8200605B740Ac6A76E";
+export const PRESALE_CA = "0x484B92a9a5493Cd190cE4b0A2A26894EB76dDE00";
 
 export const presaleContractConfig = {
   address: PRESALE_CA as Address,
